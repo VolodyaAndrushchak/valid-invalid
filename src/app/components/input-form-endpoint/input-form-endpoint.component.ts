76e0d3493f20e2090f974415370752767af8f526
Volodymyr Andrushchak <volodya1andrushchak@gmail.com>
2019-03-15 18:05:35 +0200

    rewrite the main set function

--- src/app/components/input-form-endpoint/input-form-endpoint.component.ts
+++ src/app/components/input-form-endpoint/input-form-endpoint.component.ts
@@ -20,6 +20,7 @@ export class InputFormEndpointComponent implements OnInit {
   private methods: Array<Method> = environment.methods;
   private promiseRequest: Array<Observable<null>> = [];
   private isJsonValid: boolean = true;
+  private bodyOrQuery: string;
   constructor(
     private _http: HttpRequestsService,
     private _reqRes: ReqResOperationService
@@ -74,42 +75,42 @@ private changeMethod() {
 
   private setInputData() {
     this.testCases = [];
+
     if (this.inputData.value.method === 'put' || this.inputData.value.method === 'post') {
-      this.bodyQueryValidation(this.inputData.value.body);
-
-      let originBody = JSON.parse(this.inputData.value.body.replace(/\r?\n|\r/g, ''));
-      this.testCases.push({body: originBody, state: environment.HTTP_BODY_STATE.ORIGIN});
-
-      if (!this.inputData.value.isMainDataOnly) {
-        this.testCases = this.testDataGeneration(this.testCases, originBody, 'body', 'body');
-      }
-      this.testCases.forEach(({body}, i) => {
-        this.promiseRequest.push(this._http.callHttpMethod(this.inputData.value.method, this.inputData.value.url, body, undefined));
-      });
+      this.bodyOrQuery = 'body';
     } else {
-      this.bodyQueryValidation(this.inputData.value.queryParams);
-
-      let originQuery = JSON.parse(this.inputData.value.queryParams.replace(/\r?\n|\r/g, ''));
-      this.testCases.push({query: originQuery, state: environment.HTTP_BODY_STATE.ORIGIN});
-
-      if (!this.inputData.value.isMainDataOnly) {
-        this.testCases = this.testDataGeneration(this.testCases, originQuery, 'query', 'queryParams');
-      }
-      this.testCases.forEach(({query}, i) => {
-        this.promiseRequest.push(this._http.callHttpMethod(this.inputData.value.method, this.inputData.value.url + this.serialize(query), undefined, query));
-      });
+      this.bodyOrQuery = 'queryParams';
     }
+
+    this.bodyQueryValidation(this.inputData.value[this.bodyOrQuery]);
+    let parsedBodyOrQuery = JSON.parse(this.inputData.value[this.bodyOrQuery].replace(/\r?\n|\r/g, ''));
+    let itemCase = {state: environment.HTTP_BODY_STATE.ORIGIN};
+    itemCase[this.bodyOrQuery] = parsedBodyOrQuery;
+    this.testCases.push(itemCase);
+
+    if (!this.inputData.value.isMainDataOnly) {
+      parsedBodyOrQuery = JSON.parse(this.inputData.value[this.bodyOrQuery].replace(/\r?\n|\r/g, ''));
+      this.testCases = this.testDataGeneration(this.testCases, parsedBodyOrQuery, this.bodyOrQuery);
+    }
+
+    this.testCases.forEach((item, i) => {
+      this.promiseRequest.push(this._http.callHttpMethod(
+        this.inputData.value.method, 
+        item.queryParams ? this.inputData.value.url + this.serialize(item.queryParams) : this.inputData.value.url,
+        item.body ? item.body: undefined, 
+        item.queryParams ? item.queryParams: undefined
+        ));
+    });
 
     forkJoin(this.promiseRequest).subscribe(res => {
       this._reqRes.reqResPutPostData = res;
     }, err => {}); 
   }
 
-  private testDataGeneration(testCases, data, propertyName, formName) {
-    let obj = {};
-    obj[propertyName] = {};
-    obj['state'] = environment.HTTP_BODY_STATE.GENERETED
-    testCases.push(obj);
+  private testDataGeneration(testCases, data, propertyName) {
+    let emptyBody = {state: environment.HTTP_BODY_STATE.GENERETED};
+    emptyBody[propertyName] = {};
+    testCases.push(emptyBody);
 
     for (const property in data) {
       if (data.hasOwnProperty(property)) {
@@ -120,7 +121,7 @@ private changeMethod() {
           testCases.push(testObj);
         })
       }
-      data = JSON.parse(this.inputData.value[formName].replace(/\r?\n|\r/g, ''));
+      data = JSON.parse(this.inputData.value[propertyName].replace(/\r?\n|\r/g, ''));
     }
 
     let exrtaObj = {};
@@ -137,10 +138,10 @@ private changeMethod() {
   }
 
   private serialize(obj) {
-    var str = [];
-    for (var p in obj)
+    let str = [];
+    for (let p in obj)
       if (obj.hasOwnProperty(p)) {
-        var value = obj[p];
+        const value = obj[p];
         if(obj[p] instanceof Array || obj[p] instanceof Object){
           str.push(encodeURIComponent(p) + "=" + JSON.stringify(obj[p]));
         } else {
